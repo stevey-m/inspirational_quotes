015440500220566787ea85260fd19406b2dd89ca
stevey-m <stevemcevoy07@gmail.com>
2020-11-29 20:59:30 -0500

    update

--- auto_install_script_ins_quotes.sh
+++ auto_install_script_ins_quotes.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-yum update -y
-yum install git -y
-yum install python3 -y
-git clone https://github.com/satyajitsa-code/inspquote_ws
-yum install python3-pip -y
-cd inspquote_ws
+dnf update -y
+dnf install -y git python3
+git clone https://github.com/stevey-m/inspirational_quotes
+dnf install -y python3-pip
+cd inspirational_quotes
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 iq_ws.py iq_ws.csv
+python3 inspirational_quotes.py quotes_output.csv
