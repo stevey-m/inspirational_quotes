7bcf95c9863484e6abae249d2ab1b930741fc8f2
stevey-m <stevemcevoy07@gmail.com>
2020-11-29 23:45:37 -0500

    final script

--- auto_install_script_ins_quotes.sh
+++ auto_install_script_ins_quotes.sh
@@ -5,14 +5,22 @@ git clone https://github.com/stevey-m/inspirational_quotes
 cd inspirational_quotes
 python3 -m venv venv
 source venv/bin/activate
+echo
+echo "-------------------------------"
+echo "--- PIP Install and Upgrade ---"
+echo "-------------------------------"
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 echo
+echo "----------------------"
 echo "--- Running Script ---"
+echo "----------------------"
 echo
 python3 inspirational_quotes.py quotes_output.csv
 echo
+echo "--------------------------------------"
 echo "--- Top 3 lines of the output file ---"
+echo "--------------------------------------"
 echo
 head -3 quotes_output.csv
 echo
